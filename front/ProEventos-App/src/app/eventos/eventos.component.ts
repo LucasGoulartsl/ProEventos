0ebc3bcae82fb03c378feaa7bbf0e2163065cc5a
Lucas Goulart <lucas.goulart@blueintra.com.br>
2025-08-08 10:44:48 -0300

    Adicionado FrontEnd

---
+++ front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  standalone: false,
+  templateUrl: './eventos.component.html',
+  styleUrl: './eventos.component.scss'
+})
+export class EventosComponent implements OnInit {
+
+
+  public eventos: any;
+
+  constructor( private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+   public getEventos(): void{
+    this.http.get('http://localhost:5000/api/eventos').subscribe(
+      response => this.eventos = response,
+      error => console.log(error)
+    );
+  }
+
+}
+
